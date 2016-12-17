4ef32657400ebae6966a88581d25e4773f2ff06a
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-12-17 08:37:26 -0300

    Updating slides

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,12 @@
 for (var i = 0; i < array.length; i++) {
-  array[i]
+  array[i];
+}
+
+for (var i = 0; i < array.length; i++) {
+  array[i];
+}
+
+
+for (var i = 0; i < array.length; i++) {
+  array[i];
 }
